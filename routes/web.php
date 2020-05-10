0a5a0121d4aa0fd58d20251a9172160476802a60
mzmt <mrcrius1355@gmail.com>
2020-05-10 14:34:18 +0900

    devide admin / api

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,23 @@
 use Illuminate\Support\Facades\Route;
 
 // Web routes
-Route::resource('items', 'ItemController', ['only' => ['index', 'show']]);
+Route::prefix('api')->group(function() {
+    Route::resource(
+        'items',
+        'Api\ItemController',
+        ['only' => ['index', 'show']]
+    );
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 // CSM routes
-// Route::namespace('admin')->group(function() {
-// });
+Route::prefix('admin')->group(function() {
+    Route::resource(
+        'items',
+        'Admin\ItemController',
+        ['only' => ['index', 'show', 'create', 'edit', 'update', 'destroy']]
+    );
+});
